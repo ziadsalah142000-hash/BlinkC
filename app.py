e1e2a6f0e493b30ca1d5e657eda0597c33afc1fe
ziadsalah142000-hash <ziadsalah142000@gmail.com>
2026-06-02 04:21:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Graduation Project Running Successfully"
+    return {
+        "status": "running",
+        "project": "BlinkC"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
